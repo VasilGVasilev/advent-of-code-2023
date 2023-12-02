32ca8730cfafcd0ff0c45892780a23f7f3e8505a
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-12-03 01:00:50 +0200

    procedural approach

--- day-1/Trebuchet.js
+++ day-1/Trebuchet.js
@@ -1,50 +1,46 @@
 const fs = require('fs');
 
-const lines = fs.readFileSync('input.txt', {encoding: 'utf-8'}).split('\r\n');
+const lines = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\r\n');
 
 
 
 
 // to have the first and last number we need to have to algorithms checking for numbers in each direction
 
-let sum = 0;
+console.log(getFinalSumByAdding());
 
 
-for (let i = 0; i < lines.length; i++){
-    let eachLineNumber;
+function getFinalSumByAdding() {
 
-    let leftToRightNumber;
-    for (let j = 0; j < lines[i].length; j++ ){
+    let allNumbersRefined = [];
 
-        if(isNumeric(lines[i][j])){
-            leftToRightNumber = lines[i][j];
-            break;
-        }
-    }
+    lines.map(line => allNumbersRefined.push(Number(getEachLineNumber(line))))
 
-    
-    let rightToLeftNumber;
-    for (let j = lines[i].length - 1; j >= 0; j-- ){
-
-        if(isNumeric(lines[i][j])){
-            rightToLeftNumber = lines[i][j];
-            break;
-        }
-    }
-
-    eachLineNumber = leftToRightNumber.concat(rightToLeftNumber);
-
-    sum += Number(eachLineNumber);
+    return allNumbersRefined.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
 }
 
-console.log(sum);
 
-function isNumeric(str) {
-    for(let i = 0; i < str.length; i++) {
-      const ascii = str.charCodeAt(i);
-      if(ascii < 48 || ascii > 57) {
-        return false;
-      }
+function getEachLineNumber(line) {
+
+    return (getLeftToRightNumber(line)).concat(getRightToLeftNumber(line));
+
+}
+
+function getLeftToRightNumber(line) {
+    return line.split('').find(eachChar => isNumeric(eachChar))
+}
+
+function getRightToLeftNumber(line) {
+    return line.split('').findLast(eachChar => isNumeric(eachChar))
+}
+
+function isNumeric(element) {
+    const ascii = element.charCodeAt(0);
+
+    if (ascii >= 48 && ascii <= 57) {
+        return element;
     }
-    return true;
-  }
+
+}

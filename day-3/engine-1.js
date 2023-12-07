13dfcb21a87e6ad5a72423d6000394a0592f2728
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-12-07 18:30:18 +0200

    Day 3 cannot check edge cases anymore

--- day-3/engine-1.js
+++ day-3/engine-1.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 
 const lines = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\r\n');
 
+// my output 538046, a bit above actual right number, cannot check for edge cases anymore
+console.log('Sum:', getSumOfNumbers())
 
 // get all lines
 // get each line
 // get each line's numbers
 // check if they have symbols around them
-// check if they have symbols within their stretch of the line ex. -234- you need to check on neighbour lines indices 0,1,2,3,4  
+// check if they have symbols within their stretch of the line ex. -234- you need to check on neighbour lines indices 0,1,2,3,4 
 
 function getSumOfNumbers() {
-    console.log(getAllLinesNumbers());
+    console.log('Nums to sum:', getAllLinesNumbers());
     return getAllLinesNumbers().reduce(
         (accumulator, currentValue) => accumulator + currentValue,
         0,
@@ -33,27 +35,22 @@ function getEachLine(line, index, array) {
         }
     }
 
-
     return validDigitsOnLine;
 }
 
 function getLineDigitsAndIndices(regex, line) {
     let lineCopy = line; //regex consumes the elements, so anti-pattern, but works
-    let matches = lineCopy.match(regex);
 
     let matchesAndIndicesOnLine = [];
+    let matchedDigit;
 
-    if (matches !== null) {
-        for (let match of matches) {
-            matchesAndIndicesOnLine.push({ [match]: line.indexOf(match) })
-        }
-
+    while ((matchedDigit = regex.exec(lineCopy)) !== null) {
+        let index = matchedDigit.index;
+        let value = matchedDigit[0];
+        matchesAndIndicesOnLine.push({ [value]: index });
     }
-    return matchesAndIndicesOnLine;
 
-    // console.log(matchesAndIndicesOnLine)
-    // console.log(line.indexOf(matches[0]));
-    // console.log(array[index-1], array[index+1]);
+    return matchesAndIndicesOnLine;
 }
 
 function isDigitValid(digit, line, index, array) {
@@ -69,15 +66,15 @@ function isDigitValid(digit, line, index, array) {
     // if surrounded by symbols on same line
     numberIsValid = validityOnSameLine(index, array, regexSymbol, line, digitValue, digitIndex, digitLength);
 
-
     // if symobol is present within -1/+1 of its lenght on upper or lower line
 
 
-    if (numberIsValid > 0) {
+    if (numberIsValid) {
         return Number(numberIsValid);
     } else {
         if (index > 0) {
             numberIsValid = validityOnUpperLine(digitValue, digitIndex, digitLength, index, array, regexSymbol);
+
             if (numberIsValid) {
                 return Number(numberIsValid);
             }
@@ -85,6 +82,7 @@ function isDigitValid(digit, line, index, array) {
 
         if (index + 1 < array.length) {
             numberIsValid = validityOnLowerLine(digitValue, digitIndex, digitLength, index, array, regexSymbol);
+
             if (numberIsValid) {
                 return Number(numberIsValid);
             }
@@ -97,54 +95,50 @@ function isDigitValid(digit, line, index, array) {
 
 function validityOnSameLine(index, array, regexSymbol, line, digitValue, digitIndex, digitLength) {
 
-    // DEMO WORKS
-    // let symbolBeforeDigit = line[digitIndex - 1];
-    // let symbolAfterDigit = line[digitIndex + digitLength];
-
-    // if (line[digitIndex - 1] !== '.' && line[digitIndex - 1] !== undefined || line[digitIndex + digitLength] !== '.' && line[digitIndex + digitLength] !== undefined) {
-    //     return digitValue;
-    // } else {
-    //     return 0;
-    // }
-
-    // DEMO NO LONGER WORKS
-    let currentLine = line;
-    let matchedSymbol;
-
-    // we need indices, not only the actual numbers
-    let matchSymbolArr = [];
-    while ((matchedSymbol = regexSymbol.exec(currentLine)) !== null) {
-        let index = matchedSymbol.index;
-        let value = matchedSymbol[0];
-        matchSymbolArr.push({[value] : index});
+    let lineForRegex = line; //regex consumes matches so make a new refernce
+    let elementBeforeDigit = digitIndex - 1;
+    let elementAfterDigit = digitIndex + digitLength;
+    if (digitIndex === 0) {
+        let symbolFound = lineForRegex[elementAfterDigit].match(regexSymbol);
+        if (symbolFound) {
+            return digitValue;
+        } else {
+            return 0;
+        }
     }
-    console.log(matchSymbolArr);
+    if ((digitIndex + digitLength) === lineForRegex.length) {
+        let symbolFound = lineForRegex[elementBeforeDigit].match(regexSymbol);
+        if (symbolFound) {
+            return digitValue;
+        } else {
+            return 0;
+        }
+    }
 
-    // indices used to ascertain the position of possibly valid digits
-    if (matchSymbolArr.length > 0) {
-        for (let matchedSymbolInstance of matchSymbolArr) {
+    if (digitIndex > 0) {
 
-            let symbolIndex = Object.values(matchedSymbolInstance)[0]
-            let startOfCheckingField = digitIndex - 1 < 0 ? digitIndex : (digitIndex - 1);
-            let endOfCheckingField = (digitIndex + digitLength) === array[index].length ? (digitIndex + digitLength) : (digitIndex + digitLength + 1);
-            if(startOfCheckingField === 0){
-                if (endOfCheckingField === symbolIndex){
-                    return digitValue;
-                }
-            } else if (startOfCheckingField > 0){
-                if ((startOfCheckingField - 1) === symbolIndex || endOfCheckingField === symbolIndex){
-                    return digitValue;
-                }
-            }
-            if(endOfCheckingField === array[index].length){
-                if ((startOfCheckingField - 1) === symbolIndex){
-                    return digitValue;
-                }
-            } else if (endOfCheckingField < array[index].length){
-                if (endOfCheckingField === symbolIndex || (startOfCheckingField - 1) === symbolIndex){
-                    return digitValue;
-                }
-            }
+        let symbolFoundBefore = lineForRegex[elementBeforeDigit].match(regexSymbol);
+        let symbolFoundAfter = lineForRegex[elementAfterDigit].match(regexSymbol);
+
+        if (symbolFoundBefore) {
+            return digitValue;
+        }
+        if (symbolFoundAfter) {
+            return digitValue;
+        }
+    }
+
+
+
+    if ((digitIndex + digitLength) < lineForRegex.length) {
+        let symbolFoundBefore = lineForRegex[elementBeforeDigit].match(regexSymbol);
+        let symbolFoundAfter = lineForRegex[elementAfterDigit].match(regexSymbol);
+
+        if (symbolFoundBefore) {
+            return digitValue;
+        }
+        if (symbolFoundAfter) {
+            return digitValue;
         }
     }
 }
@@ -152,15 +146,15 @@ function validityOnSameLine(index, array, regexSymbol, line, digitValue, digitIn
 function validityOnUpperLine(digitValue, digitIndex, digitLength, index, array, regexSymbol) {
 
     let lineAbove = array[index - 1];
-    
+
     let matchedSymbol;
 
-    // we need indices, not only the actual numbers
+    // we do this so that we take each unique digit index, regualr regex is shallow so 987 followed by 7, second digit will have the index of 98(7)
     let matchSymbolArr = [];
     while ((matchedSymbol = regexSymbol.exec(lineAbove)) !== null) {
         let index = matchedSymbol.index;
         let value = matchedSymbol[0];
-        matchSymbolArr.push({[value] : index});
+        matchSymbolArr.push({ [value]: index });
     }
 
     // indices used to ascertain the position of possibly valid digits
@@ -169,10 +163,10 @@ function validityOnUpperLine(digitValue, digitIndex, digitLength, index, array, 
 
             let symbol = Object.values(matchedSymbolInstance)[0]
             let startOfCheckingField = digitIndex - 1 < 0 ? digitIndex : (digitIndex - 1);
-            let endOfCheckingField = (digitIndex + digitLength) === array[index].length ? (digitIndex + digitLength) : (digitIndex + digitLength + 1);
+            let endOfCheckingField = (digitIndex + digitLength) === array[index].length ? (digitIndex + digitLength - 1) : (digitIndex + digitLength);
             if (startOfCheckingField <= symbol && endOfCheckingField >= symbol) {
                 return digitValue;
-            } 
+            }
         }
     }
 }
@@ -182,27 +176,25 @@ function validityOnLowerLine(digitValue, digitIndex, digitLength, index, array, 
 
 
     let lineAbove = array[index + 1];
-    
+
     // let matchedSymbol = regexSymbol.exec(lineAbove);
     let matchedSymbol;
     let matchSymbolArr = [];
     while ((matchedSymbol = regexSymbol.exec(lineAbove)) !== null) {
         let index = matchedSymbol.index;
         let value = matchedSymbol[0];
-        matchSymbolArr.push({[value] : index});
+        matchSymbolArr.push({ [value]: index });
     }
     if (matchSymbolArr.length > 0) {
         for (let matchedSymbolInstance of matchSymbolArr) {
 
             let symbol = Object.values(matchedSymbolInstance)[0]
             let startOfCheckingField = digitIndex - 1 < 0 ? digitIndex : (digitIndex - 1);
-            let endOfCheckingField = (digitIndex + digitLength) === array[index].length ? (digitIndex + digitLength) : (digitIndex + digitLength + 1);
+            let endOfCheckingField = (digitIndex + digitLength) === array[index].length ? (digitIndex + digitLength - 1) : (digitIndex + digitLength);
             if (startOfCheckingField <= symbol && endOfCheckingField >= symbol) {
                 return digitValue;
-            } 
+            }
         }
     }
 }
 
-
-console.log(getSumOfNumbers())

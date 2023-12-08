1bd3c1c89445ff909920eebcfe454f08706eae66
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-12-08 16:03:53 +0200

    Update almanac-1.py

--- day-5/almanac-1.py
+++ day-5/almanac-1.py
@@ -1,7 +1,15 @@
 seeds, *blocks = open(0).read().split("\n\n")
 
 # turn str of digits into list(array) of numbers 
-seeds = list(map(int, seeds.split(':')[1].split())) 
+seeds = list(map(int, seeds.split(':')[1].split()))
 
-print(seeds)
-print(blocks)
+for block in blocks:
+    # store ranges
+    ranges = []
+    # remove the title line of each block
+    for line in block.splitlines()[1:]:
+        # take each of the three nums on line
+        a, b, c = map(int, line.split())
+        ranges.append((a, b, c))
+        
+    print(ranges)

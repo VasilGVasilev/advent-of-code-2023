9751685b1ebf219f654ac5da4e059402cec8d0d8
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-12-16 21:26:00 +0200

    updated approach

--- day-8/haunted-1.py
+++ day-8/haunted-1.py
@@ -1,44 +1,9 @@
-lines = open(0).read().split("\n")
-lines.pop()
-lines = [x for x in lines if x != ''] # sanitize data
+steps, _, *rest = open(0).read().splitlines() # since it is steps, \n, empty, \n, rest
 
-directions = list(lines[0])
+network = {}
 
-nodes = lines[1:]
-nodesAsSteps = list(map(lambda e: e[:3], nodes))
-eachNodeDirections = list(map(lambda e: e[7:-1], nodes))
+for line in rest:
+    pos, targets = line.split(" = ")
+    network[pos] = targets[1:-1].split(', ') #get rid of brackets
 
-stepsDictionary = {} # Empty dictionary to add values into
-
-for index, value in enumerate(nodesAsSteps):
-   stepsDictionary[nodesAsSteps[index]] = eachNodeDirections[index]
-
-print(stepsDictionary)
-
-def steps(directions, nodes, nodesAsSteps):
-    totalSteps = 0
-    startingField = nodesAsSteps[0]
-    fieldWeAreOn = ''
-
-    while True:
-        if fieldWeAreOn != 'ZZZ':
-            for turn in directions:
-                fieldWeAreOn = startingField
-                possibleMoves = nodes.split(' = ')[1]
-                leftOrRight = possibleMoves[1:-1].split(', ')
-                leftDir = leftOrRight[0]
-                rightDir = leftOrRight[1]
-                print(fieldWeAreOn, leftDir, rightDir)
-
-                if turn == 'L':
-                    fieldWeAreOn = leftDir
-                    totalSteps+=1
-                if  turn == 'R':
-                    fieldWeAreOn = rightDir
-                    totalSteps+=1
-
-        else:
-            return totalSteps
-    
-print(steps(directions, nodes))
-
+print(network)
---
+++ day-8/test.py
@@ -0,0 +1,9 @@
+lines = [1,2,3]
+
+index = 0
+while True:
+    for e in lines:
+        print(index)
+        index+=1
+    if index == 9:
+        break

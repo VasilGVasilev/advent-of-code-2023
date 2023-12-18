171457354da2b58d1b0b6d8268635cef9428d7cb
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-12-18 16:31:12 +0200

    day-9 done

---
+++ day-9/mirage-1.py
@@ -0,0 +1,17 @@
+lines = open(0).read().split("\n")
+
+def extrapolate(array):
+    if all(x == 0 for x in array):
+        return 0
+    
+    deltas = [y - x for x, y in zip(array, array[1:])]
+    diff = extrapolate(deltas)
+    return array[-1] + diff
+
+total = 0
+
+for line in lines:
+    nums = list(map(int, line.split()))
+    total += extrapolate(nums)
+
+print(total)
